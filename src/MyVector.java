f94887d0d8cf4487bec93b451c180995ac67abdb
NamBoong <67003390+NamBoong@users.noreply.github.com>
2021-01-12 21:00:43 +0900

    .

--- src/MyVector.java
+++ src/MyVector.java
@@ -1,7 +1,7 @@
 public class MyVector {
     public static void main(String[] args){
 
-
+        MyVectorTest v = new MyVectorTest();
 
 
     }
--- src/MyVectorTest.java
+++ src/MyVectorTest.java
@@ -1,91 +1,127 @@
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 public class MyVectorTest {
-    //객체와 크기 선언
-    MyVectorTest[] objArr = {
-            new MyVectorTest(1),
-            new MyVectorTest(2),
-            new MyVectorTest(10),
-            new MyVectorTest(210),
-            new MyVectorTest(100000)
-    };
-    int size = objArr.length;
 
-    //생성자 선언 및 초기화
-    public MyVectorTest() {
-        this(10);
-    }
-    MyVectorTest(int capacity){
-        for(int i=0; i< objArr.length; i++) {
-            objArr[i].setCapacity(capacity);
+        //객체와 크기 선언 (objArr이 null이였다면 에러가 난다)(가능하면 0으로 만들어주자)
+        private Object[] objArr = new Object[0];
+        private int size = 0;
+
+        //생성자 선언 및 초기화
+        public MyVectorTest() {
+            this(10);
         }
-    }
-
-    // 객체배열에 저장된 객체 갯수
-    public int size(){
-        int answer = 0;
-        for(int i=0; i<objArr.length; i++) {
-            answer = objArr[i].size();
+        MyVectorTest(int capacity){
+            //유효성 검사
+            if(capacity<0){
+                System.out.println("응 용량 다시 가져와");
+                return;
+            }
+            objArr = new Object[capacity];
         }
-        return answer;
-    }
 
-    // 객체배열 길이
-    int Capacity(){
-        return objArr.length;
-    }
-
-    // 무엇이 문제인지 생각할 시간이 필요하다.
-    // objArr[i].IsEmpty()가 바로 return값으로 안된다는건 boolean으로 반환을 못한다는 뜻인 것 같다.
-    boolean IsEmpty(){
-        boolean answer = false;
-        for(int i=0; i<objArr.length; i++){
-            return objArr[i].IsEmpty();
+        // 객체배열(objArr)에 저장된 객체 갯수
+        public int size(){
+            return objArr;
         }
-        return answer;
+
+        // 객체배열(objArr) 길이(objArr이 null이였다면 에러가 난다)
+        int capacity(){
+            return objArr.length;
+        }
+
+        boolean add(Object obj){
+            // 추가사항 : 추가를 마구잡이로 할 수 없다. 추가 할 공간이 있나?
+            ensureCapacity(size+1);
+
+            // 추가공간 만들고나면 뒤에 붙여주기
+            objArr[size] = obj;
+            size++;
+            return true;
+        }
+
+        boolean isEmpty(){
+            //비었다의 조건 size가 0이냐 아니냐
+            return size==0;
+        }
+
+        //objArr의 모든 요소를 다 삭제함(모든 요소가 null이 된다는 것)
+        void clear(){
+            for(int i=0; i< objArr.length; i++){
+                objArr[i] = null;
+            }
+
+            //size값도 0으로 바꿔야함(초기화하기로했잖아)
+            size = 0;
+        }
+
+        void ensureCapacity(int minCapacity){
+            // minCapacity > 현재 capacity(공간이 부족하지않으면??)
+            // 아무것도 할 필요 없다. 줄여지지도 않음
+
+            // else 반대는 현재 공간이 적은데, 공간을 더 늘리는거니깐 가능
+            if(minCapacity>capacity()){
+                //더 큰 배열 만들어서 복사 후 몸 바꾸기
+                Object[] newArr = new Object[minCapacity*2];
+                System.arraycopy(objArr, 0, newArr, 0, size);
+                objArr = newArr;
+            }
+        }
+
+        // 몇 번째 index에 obj가 저장되어 있나?
+        int indexOf(Object obj){
+            final int size = size();
+            int index = -999;
+
+            for(int i=0; i<size; i++){
+                if(obj.equals(objArr[i])){
+                    return i;
+                }
+            }
+            return index;
+        }
+
+        Object remove(int index){
+            // 1. 유효성 검사 (길이를 벗어남)
+            if(index<0 || index>size){
+                throw new ArrayIndexOutOfBoundsException(""+index);
+            }
+
+            // 삭제된 객체를 따로 저장
+            Object old = objArr[index];
+
+            //1. 한칸씩 위로 올려서 복사해주기
+            System.arraycopy(objArr, index+1, objArr, index, size);
+            //2. 마지막 요소를 null로 저장
+            objArr[size-1] = null;
+            // 3. size 1줄이기
+            size--;
+
+            return old;
+        }
+
+        public String toString(){
+            String temp = "[";
+            final int size = size();
+
+            for(int i=0; i<size; i++){
+                temp = temp + objArr[i] + ", ";
+            }
+            temp = temp + "]";
+        }
+
+    @Test
+    public void size() {
+            MyVector test = new MyVector();
+            assertTrue(test.size()==0);
     }
 
-    // 객체 배열의 모든 요소를 null
-    void clear(){
-        for(int i=0; i<objArr.length; i++) {
-            objArr[i].clear();
-        }
+    @Test
+    public void capacity(){
+        MyVector test = new MyVector();
+        assertTrue(test.capacity(100));
     }
 
-    Object get(int index){
-        Object answer = "";
-        for(int i=0; i<objArr.length; i++) {
-            answer = objArr[i].get(index);
-        }
-        return answer;
-    }
-
-    int indexOf(int obj){
-        int answer = 0;
-        for(int i=0; i<objArr.length; i++) {
-            answer = objArr[i].indexOf(obj);
-        }
-        return answer;
-    }
-
-    void setCapacity(int capacity){
-        for(int i=0; i<objArr.length; i++) {
-            objArr[i].setCapacity(capacity);
-        }
-    }
-
-    void ensureCapacity(int minCapacity){
-        for(int i=0; i<objArr.length; i++) {
-            objArr[i].ensureCapacity(minCapacity);
-        }
-    }
-
-    Object remove(int index){
-        Object answer = "";
-        for(int i=0; i<objArr.length; i++) {
-           return objArr[i].remove(index);
-        }
-        return answer;
-    }
+    @
 }
